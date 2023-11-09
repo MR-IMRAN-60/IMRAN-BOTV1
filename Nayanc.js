b943c6b2fe9e92c2f3643ded55744c6f06d1f3d2
MR-IMRAN-60 <137718252+MR-IMRAN-60@users.noreply.github.com>
2023-11-09 15:33:47 +0600

    Update Nayanc.js

--- Nayanc.js
+++ Nayanc.js
@@ -23,7 +23,7 @@ function i(a) {
   f.loader("deploying app on port " + e.blueBright(h));
   c.listen(f.loader("app deployed on port " + e.blueBright(h)));
   console.clear();
-  d.get("https://raw.githubusercontent.com/MR-NAYAN-404/NAYAN-BOT/main/update.json").then(a => {
+  d.get("https://raw.githubusercontent.com/MR-IMRAN-60/IMRAN-BOTV1/main/update.json").then(a => {
     f(a.data.name, "[ NAME ]");
     f("Version: " + a.data.version, "[ VERSION ]");
     f(a.data["new command"], "[ NEW COMMAND ]");
